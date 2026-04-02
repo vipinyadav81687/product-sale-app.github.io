b8a246a35e0ba01814447e6b5f110581f40908ba
vipinyadav81687 <vipinyadav81687@gmail.com>
2026-04-02 18:02:22 +0530

    2apr2026

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -10,6 +10,7 @@ class AuthController extends Controller
     public function registerView()
     {
         try{
+            return view('auth.register');
 
         }
         catch(\Exception $e){
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,33 @@
 @extends('layouts.layout')
 @section('content')
 <div class="container">
-    <h1 classs="mt-2">Register</h1>
-    
+    <h1 class="mb-5 mt-2 text-center">Register</h1>
+    <form>
+
+  <div class="form-group">
+    <label>Name</label>
+    <input type="text" class="form-control" name="name" value="{{old('name')}}" placeholder="Enter Name" required>
+  </div>
+
+  <div class="form-group">
+    <label>Email address</label>
+    <input type="email" class="form-control" name="email" value="{{old('email')}}" placeholder="Enter Email" required>
+  </div>
+  <div class="form-group">
+    <label>Phone No</label>
+    <input type="tel" class="form-control" name="phone" value="{{old('phone')}}" id="phone" placeholder="Enter Phone no" required>
+  </div>
+  <div class="form-group">
+    <label>Password</label>
+    <input type="password" name="password" class="form-control" placeholder="Password" required>
+  </div>
+
+    <div class="form-group">
+    <label>Confirm Password</label>
+    <input type="password" name="password_confirmation" class="form-control" placeholder="Confirm Password" required>
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Register</button>
+</form>
 </div>
 @endsection
