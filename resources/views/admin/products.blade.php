6c0fb49e1ba5c22d41888088438c23836fc2723c
vipinyadav81687 <vipinyadav81687@gmail.com>
2026-06-06 10:17:44 +0530

    6Jun2026

--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -3,15 +3,14 @@
 @section('content')
     <h2 class="mb-4">Products</h2>
 
-   <!-- Button trigger modal -->
+    <!-- Button trigger modal -->
     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#createModal">
         Create Product
     </button>
 
 
-      <!--Create Modal -->
-    <div class="modal fade" id="createModal" tabindex="-1" role="dialog" aria-labelledby="createModalTitle"
-        aria-hidden="true">
+    <!--Create Modal -->
+    <div class="modal fade" id="createModal" tabindex="-1" role="dialog" aria-labelledby="createModalTitle" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -25,11 +24,8 @@
                     <div class="modal-body px-4 py-3">
                         <div class="form-group mb-3">
                             <label>Images:</label>
-                            <input type="file" class="form-control" name="images" placeholder="Product Images" required multiple accept=".jpg, .jpeg, .png, .web">
-                        </div>
-                        <div class="form-group mb-3">
-                            <label>Title:</label>
-                            <input type="text" class="form-control" name="title" placeholder="Enter Title" required>
+                            <input type="file" class="form-control" name="images" placeholder="Product Images" required
+                                multiple accept=".jpg, .jpeg, .png, .web">
                         </div>
                         <div class="form-group mb-3">
                             <label>Title:</label>
@@ -38,36 +34,70 @@
                         <div class="form-group mb-3">
                             <div class="row">
                                 <div class="col-sm-6">
-                                <label>Price (INR):</label>
-                                <input type="number" class="form-control" name="inr_price" placeholder="Price (INR)" required>
+                                    <label>Price (INR):</label>
+                                    <input type="number" class="form-control" name="inr_price" placeholder="Price (INR)"
+                                        required>
                                 </div>
                                 <div class="col-sm-6">
-                                <label>Price (USD):</label>
-                                <input type="number" class="form-control" name="usd_price" placeholder="Price (USD)" required>
+                                    <label>Price (USD):</label>
+                                    <input type="number" class="form-control" name="usd_price" placeholder="Price (USD)"
+                                        required>
                                 </div>
                             </div>
                         </div>
                         <div class="form-group mb-3">
                             <label>Stock:</label>
-                            <input type="number" min="1" class="form-control" name="stock"  placeholder="Enter Stock Quantity" required>
+                            <input type="number" min="1" class="form-control" name="stock"
+                                placeholder="Enter Stock Quantity" required>
                         </div>
                         <div class="form-group">
                             <label>Categories:</label>
                             <div class="dropdown w-100">
-                            <div class="dropdown-btn" onclick="toggleDropdown()">Select Options</div>
-                            <div class="dropdown-content">
-                                @foreach (getAllCategories() as $category)
-                                  <label>
-                                    <input type="checkbox" data-name="{{ $category->name }}" value="{{ $category->id}}"  onchange="updateSelected()">
-                                   {{ $category->name }}
-                                  </label>
-   
-                                @endforeach
-                            </div>
+                                <div class="dropdown-btn" onclick="toggleDropdown()">Select Options</div>
+                                <div class="dropdown-content">
+                                    @foreach (getAllCategories() as $category)
+                                        <label>
+                                            <input type="checkbox" data-name="{{ $category->name }}"
+                                                value="{{ $category->id }}" onchange="updateSelected()">
+                                            {{ $category->name }}
+                                        </label>
+                                        @if ($category->children->isNotEmpty())
+                                            @foreach ($category->children as $childCategory)
+                                                <label>
+                                                    <input type="checkbox" data-name="{{ $childCategory->name }}"
+                                                        value="{{ $childCategory->id }}" onchange="updateSelected()">
+                                                    {{ $childCategory->name }}
+                                                </label>
+                                            @endforeach
+                                        @endif
+                                    @endforeach
+                                </div>
                             </div>
                         </div>
-                
 
+                        <div class="form-group">
+                            <label>Variations</label>
+                            @foreach (getVariations() as $variation)
+                            <div class="mb-3">
+                                <label class="form-label">{{$variation->name}}</label>
+                                <select name="variations[{{ $loop->index }}][variation_value_ids]" class="form-control" multiple>
+                                    @foreach ($variation->values as $value )
+                                    <option value="{{ $value->id }}">{{ $value->value}}</option>      
+                                    @endforeach
+                                </select>
+                                <input type="hidden" name="variations[{{ $loop->index}}][variation_id]"
+                                value="{{ $variation->id}}">
+                            </div>
+                            @endforeach
+                        </div>
+                        <div class="form-group mb-3">
+                            <label>Description:</label>
+                            <textarea name="description" class="form-control" placeholder="Description" cols="30" rows="10"></textarea>
+                        </div>
+                         <div class="form-group mb-3">
+                            <label>Additional Information:</label>
+                            <textarea name="add_information" class="form-control" placeholder="Additional Information" cols="30" rows="10"></textarea>
+                        </div>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -77,34 +107,36 @@
             </div>
         </div>
     </div>
+@endsection
 
-
-    @endsection
-
-    @push('script')
+@push('script')
     <script>
-    function toggleDropdown() {
-        const dropdown = document.querySelector('.dropdown');
-        dropdown.classList.toggle('open');
-    }
+        function toggleDropdown() {
+            const dropdown = document.querySelector('.dropdown');
+            dropdown.classList.toggle('open');
+        }
 
-    function updateSelected() {
-        const checkboxes = document.querySelectorAll('.dropdown-content input[type="checkbox"]');
-        const selected = [];
-        checkboxes.forEach(checkbox => {
-            if (checkbox.checked) {
-                selected.push(checkbox.value);
+        function updateSelected() {
+            const checkboxes = document.querySelectorAll('.dropdown-content input[type="checkbox"]');
+            const selected = [];
+            const showName = [];
+
+            checkboxes.forEach(checkbox => {
+                if (checkbox.checked) {
+                    selected.push(checkbox.value);
+                    showName.push(checkbox.getAttribute('data-name'))
+                }
+            });
+            document.querySelector('.dropdown-btn').textContent = showName.length > 0 ? showName.join(', ') :
+                'Select Options';
+        }
+
+        // Close dropdown if clicked outside
+        document.addEventListener('click', function(event) {
+            const dropdown = document.querySelector('.dropdown');
+            if (!dropdown.contains(event.target)) {
+                dropdown.classList.remove('open');
             }
         });
-        document.querySelector('.dropdown-btn').textContent = selected.length > 0 ? selected.join(', ') : 'Select Options';
-    }
-
-    // Close dropdown if clicked outside
-    document.addEventListener('click', function(event) {
-        const dropdown = document.querySelector('.dropdown');
-        if (!dropdown.contains(event.target)) {
-            dropdown.classList.remove('open');
-        }
-    });
     </script>
-    @endpush
+@endpush
